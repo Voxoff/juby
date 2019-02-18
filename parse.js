4ba968fd64aba514cddbe0e48980a37322a38315
Guy Bennett-Jones <guybennettjones@hotmail.co.uk>
2019-02-18 16:47:02 +0000

    first few keywords

---
+++ parse.js
@@ -0,0 +1,82 @@
+function parseExpression(program) {
+  program = skipSpace(program);
+  let match, expr;
+  if (match = /^"([^"]*)"/.exec(program)) {
+    expr = {type: "value", value: match[1]};
+  } else if (match = /^\d+\b/.exec(program)) {
+    expr = {type: "value", value: Number(match[0])};
+  } else if (match = /^[^\s(),#"]+/.exec(program)) {
+    expr = {type: "word", name: match[0]};
+  } else {
+    throw new SyntaxError("Juby here! Unexpected syntax: " + program);
+  }
+
+  return parseApply(expr, program.slice(match[0].length));
+}
+
+function skipSpace(str) {
+  if(str == null) return str
+  return str.replace(/^\s+/g, '');
+}
+
+function parseApply(expr, program) {
+  program = skipSpace(program);
+  if (program[0] != "(") {
+    return {expr: expr, rest: program};
+  }
+
+  program = skipSpace(program.slice(1));
+  expr = {type: "apply", operator: expr, args: []};
+  while (program[0] != ")") {
+    let arg = parseExpression(program);
+    expr.args.push(arg.expr);
+    program = skipSpace(arg.rest);
+    if (program[0] == ",") {
+      program = skipSpace(program.slice(1));
+    } else if (program[0] != ")") {
+      throw new SyntaxError("Juby here! Expected ',' or ')'");
+    }
+  }
+  return parseApply(expr, program.slice(1)); }
+
+
+
+function parse(program) {
+  let {expr, rest} = parseExpression(program);
+  if (skipSpace(rest).length > 0) {
+    throw new SyntaxError("Juby here! Unexpected text after program");
+  }
+  return expr;
+}
+
+const specialForms = Object.create(null);
+
+function evaluate(expr, scope) {
+  if (expr.type == "value") {
+    return expr.value;
+  } else if (expr.type == "word") {
+    if (expr.name in scope) {
+      return scope[expr.name];
+    } else {
+      throw new ReferenceError(
+        `Juby here! Undefined binding: ${expr.name}`);
+    }
+  } else if (expr.type == "apply") {
+    let {operator, args} = expr;
+    if (operator.type == "word" &&
+        operator.name in specialForms) {
+      return specialForms[operator.name](expr.args, scope);
+    } else {
+      let op = evaluate(operator, scope);
+      if (typeof op == "function") {
+        return op(...args.map(arg => evaluate(arg, scope)));
+      } else {
+        throw new TypeError("Applying a non-function.");
+      }
+    }
+  }
+}
+
+let prog = parse(`if(true, false, true)`);
+console.log(evaluate(prog, topScope));
+
